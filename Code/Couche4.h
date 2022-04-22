08d6cc25744ab24b5ce6fb11f3630c32790292c2
Caelanh7 <60542246+Caelanh7@users.noreply.github.com>
2022-04-22 02:36:10 +0200

    Update Couche4.h

--- Code/Couche4.h
+++ Code/Couche4.h
@@ -27,7 +27,7 @@ int find_file(char* filename);
 * @brief Créé ou modifie un fichier en utilisant la table d'inodes
 * @param char* filename : Nom du fichier à écrire
 * @param file_t file : Fichier à écrire sur le système
-* @return int : 1 en cas d'erreur, 0 sinon
+* @return int : 0 en cas d'erreur, 1 sinon
 */
 void write_file(char* filename, file_t file);
 
@@ -52,17 +52,17 @@ int delete_file(char* filename);
 /*
 * @brief Ecrit un fichier du host sur le système
 * @param char* filename : Nom du fichier à transférer
-* @return void
+* @return int : 1 si le fichier a été chargé, 0 en cas d'erreur
 */
-void load_file_from_host(char* filename);
+int load_file_from_host(char* filename);
 
 
 /*
 * @brief Ecrit un fichier du système sur l'ordinateur host
 * @param char* filename : Nom du fichier à transférer
-* @return void
+* @return int : 1 si le fichier a été stocké, 0 en cas d'erreur
 */
-void store_file_to_host(char* filename);
+int store_file_to_host(char* filename);
 
 
 #endif // __COUCHE4_H__
