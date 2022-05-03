eaf8e7028ea3fa1e4c15096da6a582f31c88557f
totoleb <98408523+totoleb@users.noreply.github.com>
2022-05-03 11:38:29 +0200

    Add files via upload

---
+++ Code/Couche5.h
@@ -0,0 +1,23 @@
+#ifndef __COUCHE5_H__
+#define __COUCHE5_H__
+
+#include "header.h"
+#include "Couche1.h"
+#include "Couche2.h"
+#include "Couche3.h"
+
+
+int rm(char* nom_fichier);
+
+void listusers();
+
+void ls_l();
+
+void ls();
+
+void quit();
+
+void interprete_commande();
+
+
+#endif
